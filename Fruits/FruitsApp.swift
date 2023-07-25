fdc619a568e02f17e3fccd86e0b92ee650186bff
mavzoleinyk <itstart.com.ua@gmail.com>
2023-07-26 01:20:36 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Виталий Олейник on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
